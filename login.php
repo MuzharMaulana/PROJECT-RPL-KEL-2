fcbf752d821197c48c3e4e86a9cd4b110b9d924f
Adhan14 <adankendari2017@gmail.com>
2024-10-21 14:40:52 +0800

    update register

--- login.php
+++ login.php
@@ -114,7 +114,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <input type="password" name="password" placeholder="Password" required>
             <button type="submit">Masuk</button>
         </form>
-        <p>Belum punya akun? <a href="login.php">Buat akun disini</a></p>
+        <p>Belum punya akun? <a href="register.php">Buat akun disini</a></p>
     </div>
 </body>
 </html>
